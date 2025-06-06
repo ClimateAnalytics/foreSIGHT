2d2f977e931af0b09349141f2e93b7329e26d938
David McInerney <a1065639@adelaide.edu.au>
2025-06-06 11:45:43 +0930

    change to use 'auxInfo' list instead of 'obs' in function input/output.
    use 'modelInfo' from current model, rather than reverting to 'modelInfoList' (which does not include all info)

--- R/simClimateFromPar.R
+++ R/simClimateFromPar.R
@@ -1,10 +1,10 @@
 #################################
 
-parManager <- function(parS, SWGparameterization, datInd) {
+parManager <- function(parS, SWGparameterization, datInd, auxInfo=NULL) {
   UseMethod("parManager", parS)
 }
 
-SWGsim <- function(SWGpar, nTimes, randomTerm, obs=NULL) {
+SWGsim <- function(SWGpar, nTimes, randomTerm, auxInfo=NULL) {
   UseMethod("SWGsim", SWGpar)
 }
 
@@ -12,34 +12,48 @@ SWGsim <- function(SWGpar, nTimes, randomTerm, obs=NULL) {
 
 simClim = function(parS,               # vector of pars (will change in optim)
                    modelTag,
-                   ppTypes=NULL,
+                   modelInfo,
                    datInd,
                    randomTerm=NULL,
-                   obs=NULL){
+                   auxInfo = NULL){
   
   SWGmodel = strsplit(modelTag,'-')[[1]][[3]]
   SWGparameterization = strsplit(modelTag,'-')[[1]][[2]]
-  
-  timeStep = modelInfoList[[modelTag]]$timeStep
+
+  #timeStep = modelInfoList[[modelTag]]$timeStep
+  timeStep = modelInfo$timeStep
   if (is.null(timeStep)){
-    timeStep = obs$timeStep
+    timeStep = auxInfo$obs$timeStep
+  }
+
+#  names(parS) = modelInfoList[[modelTag]]$parNam
+  
+  if(length(parS)!=length(modelInfo$parNam)){
+    stop(paste0('parS has length ',length(parS),' but modelInfo$parNam has length ',length(modelInfo$parNam)))
   }
   
-  names(parS) = modelInfoList[[modelTag]]$parNam
+  names(parS) = modelInfo$parNam
   class(parS) = SWGmodel
-  
-  SWGpar = parManager(parS = parS, SWGparameterization = SWGparameterization, datInd=datInd[[aggNameShort[[timeStep]]]])
-  
+
+  SWGpar = parManager(parS = parS, SWGparameterization = SWGparameterization, 
+                      datInd=datInd[[aggNameShort[[timeStep]]]],
+                      auxInfo=auxInfo)
+
   class(SWGpar) = SWGmodel
   
   sim = SWGsim(SWGpar = SWGpar,
                   nTimes = datInd[[aggNameShort[[timeStep]]]]$nTimes,
                   randomTerm = randomTerm,
-                  obs=obs)
+               auxInfo=auxInfo)
 
-  simVar = modelInfoList[[modelTag]]$simVar
+  WG_calls = foreSIGHT_optimizationDiagnosticsEnv$WG_calls + 1
+  assign("WG_calls",WG_calls,envir = foreSIGHT_optimizationDiagnosticsEnv)
+  
+  simVar = modelInfo$simVar
+  ppTypes = modelInfo$ppTypes
   for (pp in ppTypes){
-    sim = runPP(sim,obs[[simVar]],pp,parS,datInd) 
+    sim = runPP(sim=sim,obs=auxInfo$obs[[simVar]],PPname=pp,parS=parS,
+                datInd=datInd[[aggNameShort[[timeStep]]]],randomTerm=randomTerm) 
   }
   
   return(sim)  
