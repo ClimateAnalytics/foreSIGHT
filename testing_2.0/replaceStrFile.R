c45d90a2a81240b77de15b58185f746160f417e3
David McInerney <dmcinern@gmail.com>
2025-06-17 11:59:16 +0930

    new files for phoenix hpc

---
+++ testing_2.0/replaceStrFile.R
@@ -0,0 +1,11 @@
+#####################################
+
+replaceStrFile = function(fname,oldStr,newStr){
+
+  x <- readLines(fname)
+  x <- gsub(oldStr,newStr,x)
+  cat(x, file=fname, sep="\n")
+}
+
+#####################################
+
