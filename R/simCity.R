bafe705a6cd1c514dea47f522a2de0b58bc472ed
David McInerney <a1065639@adelaide.edu.au>
2025-07-30 16:08:42 +0930

    fix modSimulator() to work with new times and call to simClim()

--- R/simCity.R
+++ R/simCity.R
@@ -56,15 +56,14 @@
 #' plot(sim$P[1:365])                             #Plot first year of rainfall
 #' }
 #' @export
-modSimulator<-function(datStart=NULL,
-                       datFinish=NULL,
+modSimulator<-function(datStart,
+                       datFinish,
                        modelTag=NULL,
                        parS=NULL,      #list that matches modelTag names
                        seed=NULL,
                        file=NULL,
                        IOmode="suppress"
                         ){
-
 
   #DO CHECKS - need date checks and model tag checks, par checks
   argument_check_simulator(modelTag=modelTag,datStart=datStart,datFinish=datFinish,parS=parS)
@@ -75,37 +74,46 @@ modSimulator<-function(datStart=NULL,
   simVar=sapply(X=modelInfo[modelTag],FUN=return.simVar,USE.NAMES=TRUE)       #?CREATE MODEL MASTER INFO - HIGHER LEVEL?
 
   #Manage dates
-  dates=makeDates(datStart=datStart,datFinish=datFinish)  #produces dates data.frame (year, month, day)
-  datInd=mod.get.date.ind(obs=dates,modelTag=modelTag,modelInfo=modelInfo) #Get datInd for all modelTags
+#  dates=makeDates(datStart=datStart,datFinish=datFinish)  #produces dates data.frame (year, month, day)
+  dates = seq(as.Date(datStart),as.Date(datFinish),by='days')
+  datInd=mod.get.date.ind(obs=list(times=dates),modelTag=modelTag,modelInfo=modelInfo) #Get datInd for all modelTags
 
+  #datInd = setup_datInd_agg(simAgg=obs$aggPeriod,times,timeStep,nperiod=1)
+  
+  # for(i in 1:length(modelTag)){
+  #   if(!is.null(modelInfoList[[modelTag[i]]]$timeStep)){
+  #     datInd[[modelTag[i]]]=setup_datInd_agg(simAgg=modelInfoList[[modelTag[i]]]$timeStep,times=dates,timeStep=modelInfoList[[modelTag[i]]]$timeStep,nperiod=modelInfo[[modelTag[i]]]$nperiod)
+  #   }
+  # }
+  
   #Culley 2019 new loop to add ar(1)
-  set.seed(seed)
-  for(mod in 1:length(modelTag)){
-    if(modelTag[mod]=="P-har-wgen"){
-
-      # hard coded parameters
-      ar1ParMult=0.001 # correlation between MULTIPLIER of monthly toals (not same as correlation between monthly totals) was 0.97
-      multRange=0.8 # i.e. 0.1 is +/10%, so multiplier 95% limit is 0.9 to 1.1
-
-      # translated param values needed for AR1
-      multiplierMean=1
-      sdJumpDistr=multRange/1.96*sqrt(1-ar1ParMult^2)
-      # simulation
-      X=stats::arima.sim(n = datInd[[modelTag[mod]]]$nyr*12, list(ar =ar1ParMult),sd = sdJumpDistr)
-      X=X@.Data+multiplierMean # extract data and add on mean
-      multSim=rep(NA,datInd[[modelTag[mod]]]$ndays)
-      for(iy in 1:datInd[[modelTag[mod]]]$nyr){
-        for(im in 1:12){
-          ind=datInd[[modelTag[mod]]]$i.yy[[iy]][which(datInd[[modelTag[mod]]]$i.yy[[iy]]%in%datInd[[modelTag[mod]]]$i.mm[[im]])]  # to save time this step can be pre-processed into datInd$i.yymm, a list of length 12*nyr
-          multSim[ind]=X[(iy-1)*12+im]
-        }
-      }
-      multSim<-pmax(multSim,0)
-      modelInfo[[modelTag[mod]]]$ar1=multSim
-    } else {
-      modelInfo[[modelTag[mod]]]$ar1=NULL
-    }
-  }
+  # set.seed(seed)
+  # for(mod in 1:length(modelTag)){
+  #   if(modelTag[mod]=="P-har-wgen"){
+  # 
+  #     # hard coded parameters
+  #     ar1ParMult=0.001 # correlation between MULTIPLIER of monthly toals (not same as correlation between monthly totals) was 0.97
+  #     multRange=0.8 # i.e. 0.1 is +/10%, so multiplier 95% limit is 0.9 to 1.1
+  # 
+  #     # translated param values needed for AR1
+  #     multiplierMean=1
+  #     sdJumpDistr=multRange/1.96*sqrt(1-ar1ParMult^2)
+  #     # simulation
+  #     X=stats::arima.sim(n = datInd[[modelTag[mod]]]$nyr*12, list(ar =ar1ParMult),sd = sdJumpDistr)
+  #     X=X@.Data+multiplierMean # extract data and add on mean
+  #     multSim=rep(NA,datInd[[modelTag[mod]]]$ndays)
+  #     for(iy in 1:datInd[[modelTag[mod]]]$nyr){
+  #       for(im in 1:12){
+  #         ind=datInd[[modelTag[mod]]]$i.yy[[iy]][which(datInd[[modelTag[mod]]]$i.yy[[iy]]%in%datInd[[modelTag[mod]]]$i.mm[[im]])]  # to save time this step can be pre-processed into datInd$i.yymm, a list of length 12*nyr
+  #         multSim[ind]=X[(iy-1)*12+im]
+  #       }
+  #     }
+  #     multSim<-pmax(multSim,0)
+  #     modelInfo[[modelTag[mod]]]$ar1=multSim
+  #   } else {
+  #     modelInfo[[modelTag[mod]]]$ar1=NULL
+  #   }
+  # }
   #Culley 2019 new seed generator, but this takes longer
   # seeds<-list()
   # ndays<-datInd[[modelTag[1]]]$ndays
@@ -120,59 +128,74 @@ modSimulator<-function(datStart=NULL,
   for(mod in 1:length(modelTag)){
     randomVector <- stats::runif(n=datInd[[modelTag[mod]]]$nTimes) # Random vector to be passed into weather generator to reduce runtime
     #IF CONDITIONED ON DRY-WET STATUS, populate wdStatus
-    switch(simVar[mod], #
-           "P" = {wdStatus=NULL},
-           "Temp" = {if(modelInfo[[modelTag[mod]]]$WDcondition==TRUE){
-                         wdStatus=out[["P"]]$sim
-                       }else{
-                         wdStatus=NULL
-                       }
-                    },
-           {wdStatus=NULL}  #default
-    )
-
+    # switch(simVar[mod], #
+    #        "P" = {wdStatus=NULL},
+    #        "Temp" = {if(modelInfo[[modelTag[mod]]]$WDcondition==TRUE){
+    #                      wdStatus=out[["P"]]$sim
+    #                    }else{
+    #                      wdStatus=NULL
+    #                    }
+    #                 },
+    #        {wdStatus=NULL}  #default
+    # )
+    
+    if (simVar[mod]=='P'){
+      wdStatus=NULL
+    } else {
+      if(modelInfo[[mod]]$WDcondition==TRUE){
+        wdStatus=out[["P"]]>modelInfo[[mod]]$WDthresh
+      }else{
+        wdStatus=NULL
+      }
+    }
+    
+    auxInfo = list(wdStatus=wdStatus)
+    
     #GRAB PARS RELATED TO modelTag
     parSel=as.double(parS[[modelTag[mod]]])
 
     # write data to model environment
     #----------------------------------
-    write_model_env(envir = foreSIGHT_modelEnv,
-                    modelInfo = modelInfo[[modelTag[mod]]],
-                    modelTag = modelTag[mod],
-                    datInd = datInd[[modelTag[mod]]]
-    )
+    # write_model_env(envir = foreSIGHT_modelEnv,
+    #                 modelInfo = modelInfo[[modelTag[mod]]],
+    #                 modelTag = modelTag[mod],
+    #                 datInd = datInd[[modelTag[mod]]]
+    # )
     #-----------------------------------
 
-    out[[simVar[mod]]]=switch_simulator(type=modelInfo[[modelTag[mod]]]$simVar,
-                                        parS=parSel,
-                                        modelEnv = foreSIGHT_modelEnv,
-                                        randomTerm = list(randomVector = randomVector,
-                                                          seed=seed),
-                                        wdSeries=wdStatus,
-                                        resid_ts=NULL)
+    # browser()
+    # 
+    # out[[simVar[mod]]]=switch_simulator(type=modelInfo[[modelTag[mod]]]$simVar,
+    #                                     parS=parSel,
+    #                                     modelEnv = foreSIGHT_modelEnv,
+    #                                     randomTerm = list(randomVector = randomVector,
+    #                                                       seed=seed),
+    #                                     wdSeries=wdStatus,
+    #                                     resid_ts=NULL)
     
     
-    browser()
+    timeStep = modelInfo$timeStep
     
-    out1[[simVar[mod]]]=simClim(parS=parSel,              #RAIN SELECTED
-                                   modelTag = modelTag,
-                                   ppTypes=modelInfo$ppTypes,
-                                   datInd=datInd[[modelTag]],
-                                   randomTerm = list(randomVector = randomVector,
-                                                     randomUnitNormalVector = randomUnitNormalVector,
-                                                     seed=optTest$seed),
-                                   obs=obs)
-    browser()
+    out[[simVar[mod]]]=simClim(parS=parSel,              #RAIN SELECTED
+                                   modelTag = modelTag[mod],
+                                   modelInfo=modelInfo[[mod]],
+                                   datInd=datInd[[modelTag[mod]]],
+                                   randomTerm = list(seed=seed,randomVector=randomVector),
+                                   auxInfo=auxInfo)
+    
   }  #end model loop
 
-  simDat=makeOutputDataframe(data=out,dates=dates,simVar=simVar,modelTag=modelTag[1])
+  out$times = dates
+  
+  #simDat=makeOutputDataframe(data=out,dates=dates,simVar=simVar,modelTag=modelTag[1])
 
   #WRITE TO FILE
-  if(IOmode!="suppress"){
-    utils::write.table(simDat,file=file,row.names=FALSE,quote = FALSE,sep=",")
-  }
+  # if(IOmode!="suppress"){
+  #   utils::write.table(simDat,file=file,row.names=FALSE,quote = FALSE,sep=",")
+  # }
 
-  return(simDat)
+  #return(simDat)
+  return(out)
 }
 # data(tankDat); obs=tank_obs
 # modelTag=c("P-har12-wgen","Temp-har26-wgen")
@@ -206,6 +229,8 @@ argument_check_simulator<-function(modelTag=NULL,
                                    datFinish=NULL,
                                    parS=NULL){
 
+  modelTaglist = names(modelInfoList)
+  
   #CHECKS FOR MODELTAGS
   # if (modelTag[1]=="Simple-ann") { stop("Simple scaling does not require calibration - invalid request")}
   if (anyDuplicated(modelTag)!=0) {stop("There are multiple entries of the same model tag")}
