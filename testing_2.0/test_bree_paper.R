a7cbd59cf80ca93ed3462ef3f91508305016ee02
David McInerney <dmcinern@gmail.com>
2025-09-02 09:00:04 +0930

    full run for phoenix

--- testing_2.0/test_bree_paper.R
+++ testing_2.0/test_bree_paper.R
@@ -1,14 +1,16 @@
 rm(list=ls())
 
-setwd('C:/Users/a1065639/Work/foreSIGHT/')
-controlFile = "C:/Users/a1065639/Work/foreSIGHT/testing_2.0/controlFile_brees_paper_foreSIGHTnew.json"
+#setwd('C:/Users/a1065639/Work/foreSIGHT/')
+setwd('../')
+controlFile = "testing_2.0/controlFile_brees_paper_foreSIGHTnew.json"
 
 # setwd('C:/Users/a1065639/Work/foreSIGHT1.2/foreSIGHT/')
 # attPerturb <- c("P_ann_tot_m","P_ann_seasRatio")
 # attHold <- c("P_ann_nWet_m", "P_ann_R10_m", "P_Feb_tot_m", "P_SON_dyWet_m", "P_JJA_avgWSD_m", "P_MAM_tot_m", "P_DJF_avgDSD_m", "Temp_ann_rng_m", "Temp_ann_avg_m")
 # controlFile = "C:/Users/a1065639/Work/foreSIGHT/testing_2.0/controlFile_brees_paper.json.txt"
 
-devtools::load_all()
+#devtools::load_all()
+library(foreSIGHT)
 
 attPerturb <- c("P_day_all_tot_m","P_day_all_seasRatio")
 attHold <- c("P_day_all_nWet_m", "P_day_all_R10_m", "P_day_Feb_tot_m", "P_day_SON_dyWet_m", "P_day_JJA_avgWSD_m", "P_day_MAM_tot_m", "P_day_DJF_avgDSD_m", "Temp_day_all_rng_m", "Temp_day_all_avg_m")
@@ -29,17 +31,21 @@ expSpace <- createExpSpace(attPerturb = attPerturb, attPerturbSamp = attPerturbS
 # expSpace <- createExpSpace(attPerturb = attPerturb, attPerturbSamp = attPerturbSamp, attPerturbMin = attPerturbMin, attPerturbMax = attPerturbMax, attPerturbType = attPerturbType, attHold = attHold)
 
 
-expSpace_subset <- expSpace
-expSpace_subset$targetMat <- expSpace$targetMat[1,]
+#expSpace_subset <- expSpace
+#expSpace_subset$targetMat <- expSpace$targetMat[1,]
 data("tankDat")
 time.1 = Sys.time()
 simTest <- generateScenarios(reference = tank_obs, 
-                             expSpace = expSpace_subset, 
+                             expSpace = expSpace, 
                              simLengthNyrs = 300, 
-                             numReplicates = 1,
-                             seedID = 2,
-                             controlFile = controlFile)
+                             numReplicates = 10,
+                             seedID = 1,
+                             controlFile = controlFile,
+			     cores=50)
 time.2 = Sys.time()
 print(time.2-time.1)
+
+save.image('bree_test.RData')
+
 
 plotScenarios(simTest)
