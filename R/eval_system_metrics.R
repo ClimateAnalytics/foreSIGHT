b99f04655682178d601672bf5044212f17730803
David McInerney <a1065639@adelaide.edu.au>
2025-09-11 01:16:49 +0930

    updated documentation

--- R/eval_system_metrics.R
+++ R/eval_system_metrics.R
@@ -13,7 +13,32 @@
 #' @param varNames a string vector; containing the names of the climate variables that are extracted from sim and used in system model. If \code{NULL}, then \code{varNames} determined from attribute names in \code{sim$expSpace}.
 #' @returns a list containing \code{systemPerf_base} and \code{systemPerf_obsClim}, with performance metrics for baseline stochastic climate and observed climate, respectively.
 #' @examples
-#' # XXXXXXXXXX
+#' # load dates, precip, PET and streamflow data for Scott Creek
+#' data('data_A5030502')
+#' clim_ref = list(times = data_A5030502$times,P = data_A5030502$P)  
+#' data("egScottCreekSimStoch")
+#' # observed flow
+#' Qobs = data_A5030502$Qobs
+#' # observed PET 
+#' PET = data_A5030502$PET 
+#' dates = as.Date(clim_ref$times)
+#' # calibrate GR4J parameters
+#' Param = calGR4J(dates = dates,P=clim_ref$P,PET=PET,Qobs=Qobs)
+#' # setup systemArgs and metrics
+#' systemArgs = list(dates=dates,Param=Param,PET=PET)
+#' metrics = c('meanQ','P99','P25','min3yr')
+#' eval = evaluate_system_metrics(sim=sim.stoch,clim=clim_ref,
+#'                                systemModel=GR4J_wrapper,systemArgs=systemArgs,
+#'                                metrics=metrics)
+#' par(mfrow=c(2,2),mar=c(3,4,1,1))
+#' for (m in 1:length(metrics)){
+#'   metric = metrics[m]
+#'   yAll = c(eval$systemPerf_base[[metric]],eval$systemPerf_obsClim[metric])
+#'   ylim = c(min(yAll),max(yAll))
+#'   boxplot_prob(as.vector(eval$systemPerf_base[[metric]]),ylim=ylim)
+#'   points(eval$systemPerf_obsClim[metric],col='red',pch=19)
+#'   title(metric)
+#' }
 #' @export
 evaluate_system_metrics <- function(sim, clim, systemModel, systemArgs, metrics, varNames = NULL) {
   expSpace <- sim$expSpace
